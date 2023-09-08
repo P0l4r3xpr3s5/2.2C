419ca3f060343ac14679666cf9c510bd85636a90
P0l4r3xpr3s5 <141984452+P0l4r3xpr3s5@users.noreply.github.com>
2023-09-08 23:01:34 +1000

    App.vue

---
+++ App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <RegistrationForm @user-registered="onUserRegistered" />
+    <GreetingMessage :userData="registeredUser" />
+  </div>
+</template>
+
+<script>
+import GreetingMessage from './components/GreetingMessage.vue';
+import RegistrationForm from './components/RegistrationForm.vue';
+
+export default {
+  components: {
+    RegistrationForm,
+    GreetingMessage
+  },
+  data() {
+    return {
+      registeredUser: null
+    };
+  },
+  methods: {
+    onUserRegistered(userData) {
+      this.registeredUser = userData;
+    }
+  }
+};
+</script>
