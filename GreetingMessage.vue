22e9b623b2cc73cfd3354506ba907d8d4c2601c2
P0l4r3xpr3s5 <141984452+P0l4r3xpr3s5@users.noreply.github.com>
2023-09-08 22:59:50 +1000

    GreetingMessage.vue

---
+++ GreetingMessage.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <p>{{ greetingMessage }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        userData: Object
+    },
+    computed: {
+        greetingMessage() {
+            if (this.userData) { 
+                return `Welcome, ${this.userData.name}! Thank you for registering on our shopping website.`;
+            }
+            return "";
+        }
+    }
+};
+</script>
